8026d2944c9b26ddbc58c94914b60828624d1ac7
Andrey Teslya <teslyauar@gmail.com>
2021-08-30 13:19:14 +0300

    lesson 10 final

--- tests/test_alice1.py
+++ tests/test_alice1.py
@@ -1,9 +1,23 @@
-def test_new_alice1(desktop_app_auth):
+def test_new_testcases(desktop_app_auth):
     test_name = 'hello'
     desktop_app_auth.navigate_to('Create new test')
     desktop_app_auth.create_test(test_name, 'world')
-    desktop_app_auth.navigate_to('Test cases')
+    desktop_app_auth.navigate_to('Test Cases')
     assert desktop_app_auth.test_cases.check_test_exists(test_name)
     desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
+def test_new_testcases_no_discr(desktop_app_auth):
+    test_name = 'hello'
+    desktop_app_auth.navigate_to('Create new test')
+    desktop_app_auth.create_test(test_name, '')
+    desktop_app_auth.navigate_to('Test Cases')
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
+def test_new_testcases_digits_name(desktop_app_auth):
+    test_name = 'hello'
+    desktop_app_auth.navigate_to('Create new test')
+    desktop_app_auth.create_test(test_name, 'world')
+    desktop_app_auth.navigate_to('Test Cases')
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
